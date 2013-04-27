9b0f04012f312ef7c7556a031d40e92060e290e5
Paul Chaignon <midionos@laposte.net>
2013-04-27 12:26:21 +0200

    In the FEN, slashes are now replaced by dollars.

--- Database chessok/requete_chessok.php
+++ Database chessok/requete_chessok.php
@@ -9,12 +9,12 @@ function redirectionErreur404() {
 }
 
 // Transforme le xml renvoye par chessok en JSon et l'affiche.
-function xmlToJson($xmlstr, $whiteToPlay) {
+function parserXMLToJSON($xmlstr, $whiteToPlay) {
 
 	$moves = new SimpleXMLElement($xmlstr);
 
 	// On calcule toutes les stats necessaires pour le serveur central.
-	foreach($moves->MoveList->Item as $item){
+	foreach($moves->MoveList->Item as $item) {
 		$nbWWhite = $item->WhiteWins;
 		$nbWBlack = $item->BlackWins;
 		$nbDraws = $item->Draws;
@@ -30,40 +30,40 @@ function xmlToJson($xmlstr, $whiteToPlay) {
 		
 		$movesArray[] = array('move'=>$move, 'probatowin'=>$probaToWin, 'probatonull'=>round($nbDraws/$nb, 3), 'nb'=>$nb);
 	}
-	echo json_encode($movesArray);
+	return json_encode($movesArray);
 }
 
 // Parseur de l'url.
 $input = file_get_contents('php://input');
-$chars = preg_split('/\//', $_SERVER['REQUEST_URI'], 3, PREG_SPLIT_NO_EMPTY); // Remplacer 3 par -1 (pour gerer les slahs).
+$chars = preg_split('/\//', $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
 
 if(count($chars)==3) {
 	if($chars[1]=='openings') {
 		// Interrogation du site chessok et recuperation des coups.
-		$fen = rawurlencode(rawurldecode($chars[3]));
+		$fen = urldecode($chars[2]);
+		$fen = str_replace('$', '/', $fen);
 		
 		// On regarde qui va jouer (blancs ou noirs).
-		preg_match("/^[^ ]* ([bw]) .*$/", rawurldecode($fen), $matches);
+		preg_match("/^[^ ]* ([bw]) .*$/", $fen, $matches);
 		if(count($matches)==2) {
 			$whiteToPlay = ($matches[1]=='w');
 		}
 		
 		// On coupe la chaine jusqu'au dernier tiret.
 		$fen = substr($fen, 0, strpos($fen, '-')+1);
-		$query = 'fen='.$fen;
+		$query = 'fen='.rawurlencode($fen);
 		
-		$curlRequest = curl_init();
+		$curlRequest = curl_init('http://chessok.com/onlineserv/opening/connection.php?timestamp='.time());
 		curl_setopt($curlRequest, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=utf-8", "Content-length: ".strlen($query), "Connection: close"));
-		curl_setopt($curlRequest, CURLOPT_URL, 'http://chessok.com/onlineserv/opening/connection.php?timestamp='.time());
-		curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($curlRequest, CURLOPT_POST, 1); 
+		curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curlRequest, CURLOPT_POST, true);
 		curl_setopt($curlRequest, CURLOPT_POSTFIELDS, $query);
 
 		$result = curl_exec($curlRequest);
 		curl_close($curlRequest);
 		
-		header("Content-Type: application/json");
-		xmlToJson($result, $whiteToPlay);
+		//header("Content-Type: application/json");
+		echo parserXMLToJSON($result, $whiteToPlay);
 		
     } else {
     	redirectionErreur404();
