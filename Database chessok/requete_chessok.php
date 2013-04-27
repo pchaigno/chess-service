c440282358b69229927bcc27b9d992dd1c69dd79
Paul Chaignon <midionos@laposte.net>
2013-04-27 09:59:52 +0200

    Special characters suppressed. It was that or encoding the file in UTF-8.

--- Database chessok/requete_chessok.php
+++ Database chessok/requete_chessok.php
@@ -1,58 +1,60 @@
 <?php
 
-$version = "1.0"; //constante
+$version = '1.0'; // Constante
 
-// fonction retournant une erreur 404
+// Fonction retournant une erreur 404.
 function redirectionErreur404() {
     header('HTTP/1.0 404 Not Found');
     exit;
 }
 
-// Transforme le xml renvoyé par chessok en JSon et l'affiche
+// Transforme le xml renvoye par chessok en JSon et l'affiche.
 function xmlToJson($xmlstr, $whiteToPlay) {
 
-$moves = new SimpleXMLElement($xmlstr);
+	$moves = new SimpleXMLElement($xmlstr);
 
-//On calcule toutes les stats nécessaires pour le serveur central.
-foreach($moves->MoveList->Item as $item){
-	$nbWWhite = $item->WhiteWins;
-	$nbWBlack = $item->BlackWins;
-	$nbDraws = $item->Draws;
-	$nb=$nbWWhite+$nbWBlack+$nbDraws;
-	//cast pour eviter de se retouver avec un SimpleXMLObject au lieu d'un string
-	$move = (string)($item->Move);
-	
-	if($whiteToPlay)
-		$probaToWin=round($nbWWhite/$nb,3);
-	else
-		$probaToWin=round($nbWBlack/$nb,3);
-	
-	$movesArray[]=array("move"=>$move, "probatowin"=>$probaToWin, "probatonull"=>round($nbDraws/$nb,3), "nb"=>$nb);
-}
-echo json_encode($movesArray);
+	// On calcule toutes les stats necessaires pour le serveur central.
+	foreach($moves->MoveList->Item as $item){
+		$nbWWhite = $item->WhiteWins;
+		$nbWBlack = $item->BlackWins;
+		$nbDraws = $item->Draws;
+		$nb = $nbWWhite + $nbWBlack + $nbDraws;
+		// Cast pour eviter de se retouver avec un SimpleXMLObject au lieu d'un string.
+		$move = (string)($item->Move);
+		
+		if($whiteToPlay) {
+			$probaToWin = round($nbWWhite/$nb,3);
+		} else {
+			$probaToWin = round($nbWBlack/$nb,3);
+		}
+		
+		$movesArray[]=array('move'=>$move, 'probatowin'=>$probaToWin, 'probatonull'=>round($nbDraws/$nb,3), 'nb'=>$nb);
+	}
+	echo json_encode($movesArray);
 }
 
-// parseur de l'url    
+// Parseur de l'url.
 $input = file_get_contents('php://input');
-$chars = preg_split('/\//', $_SERVER['REQUEST_URI'], 4, PREG_SPLIT_NO_EMPTY);//remplacer 4 par -1 (pour gérer les slahs)
+$chars = preg_split('/\//', $_SERVER['REQUEST_URI'], 4, PREG_SPLIT_NO_EMPTY); // Remplacer 4 par -1 (pour gerer les slahs).
 
 if(count($chars)==4 && $chars[1]=='rest') {
 	if($chars[2]=='openings') {
-		// interrogation du site chessok et recuperation des coups
-		$fen=rawurlencode(rawurldecode($chars[3]));
+		// Interrogation du site chessok et recuperation des coups.
+		$fen = rawurlencode(rawurldecode($chars[3]));
 		
-		//On regarde qui va jouer (blancs ou noirs)
+		// On regarde qui va jouer (blancs ou noirs).
 		preg_match("/^[^ ]* ([bw]) .*$/", rawurldecode($fen), $matches);
-		if(count($matches)==2)
-			$whiteToPlay = ($matches[1]=="w");
+		if(count($matches)==2) {
+			$whiteToPlay = ($matches[1]=='w');
+		}
 		
-		//On coupe la chaine jusqu'au dernier tiret
-		$fen=substr($fen, 0, strpos($fen, "-")+1);
-		$query="fen=".$fen;
+		// On coupe la chaine jusqu'au dernier tiret.
+		$fen = substr($fen, 0, strpos($fen, '-')+1);
+		$query = 'fen='.$fen;
 		
 		$curlRequest = curl_init();
 		curl_setopt($curlRequest, CURLOPT_HTTPHEADER, array("Content-Type: application/x-www-form-urlencoded; charset=utf-8", "Content-length: ".strlen($query), "Connection: close"));
-		curl_setopt($curlRequest, CURLOPT_URL, "http://chessok.com/onlineserv/opening/connection.php?timestamp=".time());
+		curl_setopt($curlRequest, CURLOPT_URL, 'http://chessok.com/onlineserv/opening/connection.php?timestamp='.time());
 		curl_setopt($curlRequest, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($curlRequest, CURLOPT_POST, 1); 
 		curl_setopt($curlRequest, CURLOPT_POSTFIELDS, $query);
@@ -63,13 +65,12 @@ if(count($chars)==4 && $chars[1]=='rest') {
 		header("Content-Type: application/json");
 		xmlToJson($result, $whiteToPlay);
 		
-    }
-    else {
+    } else {
     	redirectionErreur404();
 	}
-        
+      
 } elseif(count($chars)==3 && $chars[1]=='rest' && $chars[2]=='version') {
-	//envoie du numéro de version
+	// Envoie du numero de version
 	echo $version;
 } else {
 	redirectionErreur404();
@@ -78,3 +79,5 @@ if(count($chars)==4 && $chars[1]=='rest') {
 //RewriteEngine On
 //RewriteCond %{REQUEST_URI} ^/rest/.*$ [NC]
 //RewriteRule ^(.*)$ /requete_chessok.php [L]
+
+?>
--- Wrapper Bases de donnees/chessgame.php
+++ Wrapper Bases de donnees/chessgame.php
@@ -1,27 +1,27 @@
 <?php
 
-$version = "1.0s"; //constante
+$version = '1.0s'; // Cconstante
 
-// fonction retournant une erreur 404
+// Fonction retournant une erreur 404.
 function redirectionErreur404() {
     header('HTTP/1.0 404 Not Found');
     exit;
 }
 
-// ouverture de la base de données contenant les tables d'ouverture et de fermeture
+// Ouverture de la base de données contenant les tables d'ouverture et de fermeture.
 try {
 	$bdd = new PDO('mysql:host=localhost;dbname=chessgames', 'root', 'root');
 } catch(PDOException $e) {
 	exit('Error: '.$e->getMessage());
 }
 
-// parseur de l'url    
+// Parseur de l'url.
 $input = file_get_contents('php://input');
 $chars = preg_split('/\//', $_SERVER['REQUEST_URI'], -1, PREG_SPLIT_NO_EMPTY);
 
 if(count($chars)==3) {
 	if($chars[1]=='openings') {
-		// selection selon le fen	
+		// Selection selon le fen.
 		try {
 			$moves = $bdd->prepare("SELECT move, probatowin, probatonull, nb FROM openings WHERE fen LIKE ?");
 			$moves->execute(array($chars[2]));
@@ -29,12 +29,12 @@ if(count($chars)==3) {
 			exit('Error: '.$e->getMessage());
 		}	
 		header("Content-Type: application/json");
-		//la boucle while est enlevée pour assurer la compatibilité avec Gson
+		// La boucle while est enlevee pour assurer la compatibilite avec Gson.
 		$arrayMoves = $moves->fetchAll();
 		echo json_encode($arrayMoves);
 		
     } elseif($chars[1]=='endings') {
-		//selection selon le fen
+		// Selection selon le fen.
 		try {
 			$moves = $bdd->prepare("SELECT move, probatowin, probatonull, nb FROM endings WHERE fen LIKE ?");
 			$moves->execute(array($chars[2]));
@@ -42,7 +42,7 @@ if(count($chars)==3) {
 			exit('Error: '.$e->getMessage());
 		}
 		header("Content-Type: application/json");
-		//la boucle while est enlevée pour assurer la compatibilité avec Gson
+		// La boucle while est enlevee pour assurer la compatibilite avec Gson.
 		$arrayMoves = $moves->fetchAll();
 		echo json_encode($arrayMoves);
 		
@@ -51,7 +51,7 @@ if(count($chars)==3) {
 	}
         
 } elseif(count($chars)==2 && $chars[1]=='version') {
-	//envoie du numéro de version
+	// Envoie du numéro de version
 	echo $version;
 } else {
 	redirectionErreur404();
