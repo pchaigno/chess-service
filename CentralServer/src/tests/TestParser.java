984ed0afcf116c798cd87439821732317e50818a
Midionos <midionos@laposte.net>
2013-05-06 19:39:15 +0200

    New unit tests of the notations parser.

--- CentralServer/src/tests/TestParser.java
+++ CentralServer/src/tests/TestParser.java
@@ -11,12 +11,26 @@ import junit.framework.TestCase;
 public class TestParser extends TestCase {
 
 	/**
-	 * Test a few conversions from LAN to SAN or the inverse.
+	 * Test conversions from LAN to SAN.
 	 */
-	public void testNotationsParser() {
+	public void testSANParser() {
 		this.testLANToSAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "b1c3");
-		this.testSANToLAN("rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3", "Nf6");
 		this.testLANToSAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "d2d4");
+		this.testLANToSAN("rnbqkbnr/ppp1pp1p/6p1/3p4/5P2/3P1N2/PPP1P1PP/RNBQKB1R b KQkq - 1 3", "f8g7", "Bg7");
+	}
+	
+	/**
+	 * Test conversions from SAN to LAN.
+	 */
+	public void testLANParser() {
+		this.testSANToLAN("rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR b KQkq d3", "Nf6");
+		this.testSANToLAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "d4");
+		this.testSANToLAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "d3");
+		this.testSANToLAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "Nf3");
+		this.testSANToLAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "Qe2", "d1e2");
+		this.testSANToLAN("rnbqkbnr/pppp1ppp/4p3/8/4P3/8/PPPP1PPP/RNBQKBNR w KQkq - 0 2", "Nc3");
+		this.testSANToLAN("rnbqkbnr/pp1ppppp/2p5/8/2P1P3/8/PP1P1PPP/RNBQKBNR b KQkq - 0 2", "d5");
+		this.testSANToLAN("rnbqkbnr/ppp1pp1p/6p1/3p4/5P2/3P1N2/PPP1P1PP/RNBQKB1R b KQkq - 1 3", "Bg7");
 	}
 	
 	/**
@@ -41,6 +55,17 @@ public class TestParser extends TestCase {
 	}
 	
 	/**
+	 * Test a conversion from SAN to LAN.
+	 * @param fen The FEN.
+	 * @param san The Short Algebraic Notation.
+	 * @param lan The Long Algebraic Notation.
+	 */
+	private void testSANToLAN(String fen, String san, String lan) {
+		ChessParser parser = new ChessParser(fen);
+		assertEquals(lan, parser.convertSANToLAN(san));
+	}
+	
+	/**
 	 * Test a conversion from LAN to SAN.
 	 * @param fen The FEN.
 	 * @param lan The Long Algebraic Notation.
@@ -49,6 +74,17 @@ public class TestParser extends TestCase {
 		ChessParser parser = new ChessParser(fen);
 		String san = parser.convertLANToSAN(lan);
 		assertEquals(lan, parser.convertSANToLAN(san));
+	}
+	
+	/**
+	 * Test a conversion from LAN to SAN.
+	 * @param fen The FEN.
+	 * @param lan The Long Algebraic Notation.
+	 * @param san The Short Algebraic Notation.
+	 */
+	private void testLANToSAN(String fen, String lan, String san) {
+		ChessParser parser = new ChessParser(fen);
+		assertEquals(san, parser.convertLANToSAN(lan));
 	}
 	
 	/**
