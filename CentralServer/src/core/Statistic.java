1b72fe18b03363aafaedfab5621d0a9432c28e59
clemgaut <clemgaut@gmail.com>
2013-05-19 21:14:14 +0200

    Factorisation du code dans StatsManager

--- CentralServer/src/core/Statistic.java
+++ CentralServer/src/core/Statistic.java
@@ -1,31 +1,76 @@
 package core;
 
 /**
- * Enumeration of statistics used by StatsManager.
+ * Contain the statistics used by StatsManager.
+ * Contain the enumeration of all Statistics accessible.
  * @author Clement Gautrais
  */
-public enum Statistic {
-	MEAN (0),
-	VARIANCE (1),
-	WEIGHT (2),
-	NORMALIZATION_MEAN (3),
-	NORMALIZATION_VARIANCE (4);
+public class Statistic {
 	
-	private int value;
-	
-	/**
-	 * Constructor
-	 * @param value The numeric value corresponding to the type of statistic.
-	 */
-	Statistic(int value) {
-		this.value = value;
+	public enum Stat{
+		WEIGHT,
+		MEAN,
+		VARIANCE,
+		NORMALIZATION_MEAN,
+		NORMALIZATION_VARIANCE;
 	}
 	
-	/**
-	 * @return The value corresponding to the type of statistic.
-	 */
-	public int getValue() {
-		return this.value;
+	private double mean;
+	private double variance;
+	private double normalization_mean;
+	private double normalization_variance;
+	
+	public Statistic(){
+		init();
+	}
+	
+	
+	public Statistic(double mean, double variance, double normalization_mean, double normalization_variance) {
+		super();
+		this.mean = mean;
+		this.variance = variance;
+		this.normalization_mean = normalization_mean;
+		this.normalization_variance = normalization_variance;
+	}
+
+
+	public void init(){
+		mean = 0;
+		variance = 0;
+		normalization_mean = 0;
+		normalization_variance = 0;
+	}
+
+	public double getMean() {
+		return mean;
+	}
+
+	public double getVariance() {
+		return variance;
+	}
+
+	public double getNormalization_mean() {
+		return normalization_mean;
+	}
+
+	public double getNormalization_variance() {
+		return normalization_variance;
+	}
+
+	public void setMean(double mean) {
+		this.mean = mean;
+	}
+
+	public void setVariance(double variance) {
+		this.variance = variance;
+	}
+
+	public void setNormalization_mean(double normalization_mean) {
+		this.normalization_mean = normalization_mean;
+	}
+
+	public void setNormalization_variance(double normalization_variance) {
+		this.normalization_variance = normalization_variance;
 	}
 	
 }
