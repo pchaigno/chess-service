ee29d1eca222206153c388db54f0d09d065a9bf4
Paul Chaignon <midionos@laposte.net>
2013-05-19 22:20:24 +0200

    Code simplification.

--- CentralServer/src/core/Statistic.java
+++ CentralServer/src/core/Statistic.java
@@ -7,7 +7,7 @@ package core;
  */
 public class Statistic {
 	
-	public enum Stat{
+	enum Stat {
 		WEIGHT,
 		MEAN,
 		VARIANCE,
@@ -15,62 +15,26 @@ public class Statistic {
 		NORMALIZATION_VARIANCE;
 	}
 	
-	private double mean;
-	private double variance;
-	private double normalization_mean;
-	private double normalization_variance;
+	double mean;
+	double variance;
+	double normalizationMean;
+	double normalizationVariance;
 	
-	public Statistic(){
+	public Statistic() {
 		init();
 	}
 	
-	
-	public Statistic(double mean, double variance, double normalization_mean, double normalization_variance) {
-		super();
+	public Statistic(double mean, double variance, double normalizationMean, double normalizationVariance) {
 		this.mean = mean;
 		this.variance = variance;
-		this.normalization_mean = normalization_mean;
-		this.normalization_variance = normalization_variance;
-	}
-
-
-	public void init(){
-		mean = 0;
-		variance = 0;
-		normalization_mean = 0;
-		normalization_variance = 0;
-	}
-
-	public double getMean() {
-		return mean;
-	}
-
-	public double getVariance() {
-		return variance;
-	}
-
-	public double getNormalization_mean() {
-		return normalization_mean;
-	}
-
-	public double getNormalization_variance() {
-		return normalization_variance;
-	}
-
-	public void setMean(double mean) {
-		this.mean = mean;
-	}
-
-	public void setVariance(double variance) {
-		this.variance = variance;
-	}
-
-	public void setNormalization_mean(double normalization_mean) {
-		this.normalization_mean = normalization_mean;
-	}
-
-	public void setNormalization_variance(double normalization_variance) {
-		this.normalization_variance = normalization_variance;
+		this.normalizationMean = normalizationMean;
+		this.normalizationVariance = normalizationVariance;
 	}
 	
+	public void init() {
+		this.mean = 0;
+		this.variance = 0;
+		this.normalizationMean = 0;
+		this.normalizationVariance = 0;
+	}
 }
